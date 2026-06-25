d62530e950cda6be5333e1cbe8b92a470750e73d
Daniel <danielhu@mercuria.com>
2026-06-25 10:56:55 +0800

    edited home page

--- Home.py
+++ Home.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Home", layout="wide")
 st.title("Voyage Economics Dashboard")
-st.write("Navigate to the TCE Calculator on the left")
+st.write("Navigate to the TCE Calculator or Lumpsum Calculator on the left")
+st.write("The TCE Calculator calculates the TCE for various voyages. The Lumpsum Calculator calculates the TCE for a voyage that uses a lumpsum.")
